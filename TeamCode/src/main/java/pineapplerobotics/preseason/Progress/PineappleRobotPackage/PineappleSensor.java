e3abbc6725bf29918662886781bf3ff909e01285
krishpineapple <krishpineapple@users.noreply.github.com>
2017-08-07 14:31:01 -0400

    Update PineappleSensor.java

--- TeamCode/src/main/java/pineapplerobotics/preseason/Progress/PineappleRobotPackage/PineappleSensor.java
+++ TeamCode/src/main/java/pineapplerobotics/preseason/Progress/PineappleRobotPackage/PineappleSensor.java
@@ -9,13 +9,9 @@ import pineapplerobotics.preseason.Progress.PineappleRobotPackage.PineappleEnum;
 public abstract class PineappleSensor {
 
     public String sensorName;
-<<<<<<< HEAD
     
     public abstract void makeSensor(String name, PineappleResources pineappleResources);
-=======
 
-    abstract void makeSensor(String name);
->>>>>>> 87dc5cf2cc09643810614fcce939ae35c4cc4981
 
     public abstract double getValue();
 }
