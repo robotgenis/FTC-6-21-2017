3ef9926c02544fdb8c156d5d4c4e30a1bfc07092
Krishna Suresh <krishna4132@gmail.com>
2017-08-07 17:42:43 -0400

    CPR and encoder drive added but not tested

--- TeamCode/src/main/java/pineapplerobotics/preseason/Progress/PineappleRobotPackage/TestWithoutConfig/CustomMotorTest.java
+++ TeamCode/src/main/java/pineapplerobotics/preseason/Progress/PineappleRobotPackage/TestWithoutConfig/CustomMotorTest.java
@@ -1,9 +1,14 @@
 package pineapplerobotics.preseason.Progress.PineappleRobotPackage.TestWithoutConfig;
 
+import com.qualcomm.hardware.motors.NeveRest40Gearmotor;
+import com.qualcomm.hardware.motors.NeveRest60Gearmotor;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.configuration.MotorConfigurationType;
 
+import pineapplerobotics.preseason.Progress.PineappleRobotPackage.PineappleEnum;
 import pineapplerobotics.preseason.Progress.PineappleRobotPackage.PineappleMotor;
 import pineapplerobotics.preseason.Progress.PineappleRobotPackage.PineappleRobot;
 
@@ -17,13 +22,15 @@ public class CustomMotorTest extends LinearOpMode{
     PineappleRobot robot;
 
     PineappleMotor testMotor;
+    NeveRest60Gearmotor neveRest60Gearmotor;
+
 
     @Override
     public void runOpMode() throws InterruptedException {
 
         robot = new PineappleRobot(this);
 
-        testMotor = robot.motorHandler.newMotor("test", 1, true , true);
+        testMotor = robot.motorHandler.newMotor("test", 1, true , true, PineappleEnum.MotorType.NEV40);
 
         robot.mapRobot();
 
@@ -31,7 +38,6 @@ public class CustomMotorTest extends LinearOpMode{
 
         waitForStart();
         while (opModeIsActive()){
-
             testMotor.update(gamepad1.left_stick_x);
 
 
