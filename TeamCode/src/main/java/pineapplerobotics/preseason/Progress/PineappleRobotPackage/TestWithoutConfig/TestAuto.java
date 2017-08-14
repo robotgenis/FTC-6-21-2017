072f51b94084038c62fc4d5f5df7f23dc81fccdd
Brandon Young <robotgenis@gmail.com>
2017-08-14 13:00:55 -0400

    Update

--- TeamCode/src/main/java/pineapplerobotics/preseason/Progress/PineappleRobotPackage/TestWithoutConfig/TestAuto.java
+++ TeamCode/src/main/java/pineapplerobotics/preseason/Progress/PineappleRobotPackage/TestWithoutConfig/TestAuto.java
@@ -1,15 +1,22 @@
 package pineapplerobotics.preseason.Progress.PineappleRobotPackage.TestWithoutConfig;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import pineapplerobotics.preseason.Progress.PineappleRobotPackage.PineappleEnum;
 import pineapplerobotics.preseason.Progress.PineappleRobotPackage.PineappleMotor;
 import pineapplerobotics.preseason.Progress.PineappleRobotPackage.PineappleRobot;
+import pineapplerobotics.preseason.Progress.PineappleRobotPackage.PineappleSensors.PineappleColorSensor;
 import pineapplerobotics.preseason.Progress.PineappleRobotPackage.PineappleSensors.PineappleGyroSensor;
+import pineapplerobotics.preseason.Progress.PineappleRobotPackage.PineappleSensors.PineappleOpticalDistanceSensor;
+import pineapplerobotics.preseason.Progress.PineappleRobotPackage.PineappleSensors.PineappleTouchSensor;
 
 /**
  * Created by Brandon on 8/14/2017.
  */
+@TeleOp(name = "PineappleRobotTestAuto", group = "Linear Opmode")
 
 public class TestAuto extends LinearOpMode{
 
@@ -18,7 +25,8 @@ public class TestAuto extends LinearOpMode{
     PineappleMotor left;
     PineappleMotor right;
 
-    PineappleGyroSensor gyro;
+    PineappleOpticalDistanceSensor color;
+    PineappleTouchSensor touch;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -27,11 +35,16 @@ public class TestAuto extends LinearOpMode{
         left = robot.motorHandler.newDriveMotor("left", 2, false, false, PineappleEnum.MotorLoc.LEFT, PineappleEnum.MotorType.NEV40);
         right = robot.motorHandler.newDriveMotor("right", 2, false, false, PineappleEnum.MotorLoc.RIGHT, PineappleEnum.MotorType.NEV40);
 
-        gyro = robot.sensorHandler.newGyroSensor("g");
+        color = robot.sensorHandler.newOpticalDistanceSensor("o");
+        touch = robot.sensorHandler.newTouchSensor("t");
+
+        robot.mapRobot();
+
+        right.motorObject.setDirection(DcMotor.Direction.REVERSE);
 
         waitForStart();
 
-        robot.auto.driveUntil(gyro, PineappleEnum.PineappleSensorEnum.GSHEADING, PineappleEnum.condition.GREATERTHAN, 10, .2);
+        robot.auto.lineFollow(color, PineappleEnum.PineappleSensorEnum.ODSLIGHTDETECTED,touch, PineappleEnum.PineappleSensorEnum.TOUCH, PineappleEnum.condition.GREATERTHAN, 90, -.4);
 
     }
 }
